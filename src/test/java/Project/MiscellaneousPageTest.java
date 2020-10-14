b0a50aa6a2d23a7666f4e2003bae9fdfbf307ca3
Satya Kalyan <satyakalyan5@gmail.com>
2020-10-13 20:13:42 -0500

    Added <release> tag to handle the Jenkins Issue

--- src/test/java/Project/MiscellaneousPageTest.java
+++ src/test/java/Project/MiscellaneousPageTest.java
@@ -4,6 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,6 +16,7 @@ import java.awt.*;
 import java.awt.event.InputEvent;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MiscellaneousPageTest extends baseClass {
 
@@ -25,12 +29,13 @@ public class MiscellaneousPageTest extends baseClass {
         driver = initializeDriver();
         log.info("Driver is initialized");
         driver.manage().window().maximize();
-        driver.get(prop.getProperty("miscellaneouspage"));
-        log.info("Navigated to Miscellaneous Page");
     }
 
     @Test
     public void dragAndDropFrameElements() throws InterruptedException, AWTException, IOException {
+        driver.get(prop.getProperty("miscellaneouspage"));
+        log.info("Navigated to Miscellaneous Page");
+
         log.info("dragAndDropFrameElements script is running");
         int X_coordinate1 = 300;
         int Y_coordinate1 = 300;
@@ -76,11 +81,32 @@ public class MiscellaneousPageTest extends baseClass {
         }
     }
 
-    //Closing the driver
+    @Test
+    public void openAMEXGoBlock() throws IOException, InterruptedException {
+        driver.get("https://www.americanexpress.com/en-us/business/payment-solutions/employee-spending/");
+
+        //Because I receive a Stale Element Reference Exception here, I am using a Thread.sleep() which makes sure that I am waiting for
+        //the element to completely load in the page
+        Thread.sleep(10000);
+        driver.findElement(By.xpath("//span[2]//div[1]//div[2]//div[1]//div[1]//div[1]")).click();
+
+        //Instead of directly grabbing the text from the page using a specific XPath like 'contains(text(),...)', I verified the 'American Express Go' text in the page and then
+        //traversed through the HTML to get the text
+        String amexGotext = driver.findElement(By.xpath("//h2[contains(text(),'American Express Go')]/parent::div/parent::div/following-sibling::div/div[1]")).getText();
+        Assert.assertEquals(amexGotext, "This fast, flexible virtual payment method allows employees to make purchases. Virtual Cards are sent to users via a mobile app, and can be used online immediately.");
+
+        //The hardcoded paths used here are generally kept in POM pages and then sent over to here
+
+        //In the interview, I was handling the exception using Explicit waits and hence the error. I could have also used try-catch block but
+        //I wanted to avoid it as the time was limited
+    }
+
+//    	Closing the driver
     @AfterMethod(groups = {"must"})
     public void closeBrowser() {
         log.info("Closing the driver");
         driver.quit();
         driver = null;
     }
+
 }
